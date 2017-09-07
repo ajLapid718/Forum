4acf60e44dc318d794e5815bda094fb9f2af06ec
Allan James Lapid <ajLapid718@gmail.com>
2017-09-07 00:50:16 -0400

    Edit and Update CRUD as well as link_to in the form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,4 +10,28 @@ class CommentsController < ApplicationController
       flash[:errors] = @comment.errors.full_messages
     end
   end
+
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+
+    if @comment.update(params[:comment].permit(:comment))
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to @post
+  end
+
 end
