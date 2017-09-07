ffda0af7a95e0d1578abd55b732296cabbe5461d
Allan James Lapid <ajLapid718@gmail.com>
2017-09-07 00:09:11 -0400

    Cruddin

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
+  # def new
+  #   @comment = @post.comments.new
+  # end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(params[:comment].permit(:comment))
+
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
 end
